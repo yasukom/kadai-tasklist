49055a23c9c1972a28be51141baae097c614760a
yasuko.matsubara <yassu.mattsu247@gmail.com>
2020-11-10 10:56:47 +0000

    add validation and status column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_091112) do
+ActiveRecord::Schema.define(version: 2020_11_10_104731) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
